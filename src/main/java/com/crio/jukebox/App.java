d785d0de86cc120cc65422c9781e03c03cb1a7a5
Crio.GitLab <admin@example.com>
2021-12-22 14:28:30 +0000

    Update src/main/java/com/crio/jukebox/App.java, src/test/java/com/crio/jukebox/AppTest.java files

---
+++ src/main/java/com/crio/jukebox/App.java
@@ -0,0 +1,29 @@
+package com.crio.jukebox;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+public class App {
+    // To run the application  ./gradlew run --args="INPUT_FILE=jukebox-input.txt"
+	public static void main(String[] args) {
+		List<String> commandLineArgs = new LinkedList<>(Arrays.asList(args));
+        String expectedSequence = "INPUT-FILE";
+        String actualSequence = commandLineArgs.stream()
+                .map(a -> a.split("=")[0])
+                .collect(Collectors.joining("$"));
+        if(expectedSequence.equals(actualSequence)){
+            run(commandLineArgs);
+        }
+	}
+
+    public static void run(List<String> commandLineArgs) {
+    // Complete the final logic to run the complete program.
+
+    }
+}
