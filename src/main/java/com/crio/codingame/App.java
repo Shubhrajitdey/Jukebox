ced58a4104fc171510c189180ae5810ae0c2dd71
jnana_cetana <jnana_cetana@DESKTOP-MNGCD2S>
2022-01-17 21:29:00 +0550

    updated changes - 2022-01-17 21:09:00

--- src/main/java/com/crio/codingame/App.java
+++ src/main/java/com/crio/codingame/App.java
@@ -10,6 +10,7 @@ import java.util.stream.Collectors;
 
 
 public class App {
+    // To run the application  ./gradlew run --args="INPUT_FILE=input.txt"
     public static void main(String[] args) {
     }
 }
